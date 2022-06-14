30b17bd6d8ce14b77b4991240fe4b49f654e8fc5
dimplehh <dimplehh@naver.com>
2022-06-14 22:56:23 +0900

    팔 일정부분에선 되는데 전체에선.. 진짜 난제다..

--- Assets/Mediapipe/Samples/Scripts/Controller.cs
+++ Assets/Mediapipe/Samples/Scripts/Controller.cs
@@ -14,8 +14,9 @@ namespace Mediapipe.Unity
     public GameObject upperbody;
     public GameObject avatar;
     private static float legscale;
+    private static float armscale;
     private Vector3[] Point = new Vector3[33];
-    private float zPoint = 80f;
+    private float zPoint = 90f;
 
     // Start is called before the first frame update
     void Start()
@@ -32,12 +33,14 @@ namespace Mediapipe.Unity
     {
       targetPosition(PointListAnnotation.point);
       legscale = PointListAnnotation.legscale / 9.17f;
+      armscale = PointListAnnotation.armscale / 3.19f;
 
       Vector3 newPosition = new Vector3((Point[23].x + Point[24].x)/2, (Point[23].y + Point[24].y) / 2, zPoint);
       Vector3 newPosition2 = new Vector3((Point[11].x + Point[12].x) / 2, (Point[11].y + Point[12].y) / 2, zPoint);
       avatar.transform.position = newPosition;
 
       float scale = Vector3.Magnitude(Point[11] - Point[12]) / 25; //몸 크기를 어깨 벡터 크기 비례로
+      //Debug.Log("scale:" + scale);
       float urscale = Vector3.Magnitude(Point[11] - Point[13]) / 25;
       float drscale = Vector3.Magnitude(Point[15] - Point[13]) / 25;
       float upbodyscale = Vector3.Magnitude(newPosition - newPosition2);
@@ -48,13 +51,13 @@ namespace Mediapipe.Unity
         avatar.transform.localScale = new Vector3(scale,scale, scale);
         if(0.0f < scale && scale <= 3.0f) //이 조건 없으면 에러뜸
         {
-          upperbody.transform.localScale = new Vector3(1.2f, 1.10f * (upbodyscale / scale / 25), 1.0f);
+          upperbody.transform.localScale = new Vector3(1.2f, 1.10f * (upbodyscale / scale / 30), 1.0f); //가까이있을땐 30나누고 멀땐 25
           lupperleg.transform.localScale = new Vector3(1.0f, legscale, 1.0f);
           rupperleg.transform.localScale = new Vector3(1.0f, legscale, 1.0f);
           //ldownleg.transform.localScale = new Vector3(1.0f, dlscale / scale / 0.81f, 1.0f);
           //rdownleg.transform.localScale = new Vector3(1.0f, dlscale / scale / 0.81f, 1.0f);
-          //lupperarm.transform.localScale = new Vector3(1.0f, 0.84f * (urscale / scale), 1.0f);
-          //rupperarm.transform.localScale = new Vector3(1.0f, 0.84f * (urscale / scale), 1.0f);
+          lupperarm.transform.localScale = new Vector3(1.0f, armscale, 1.0f);
+          rupperarm.transform.localScale = new Vector3(1.0f, armscale, 1.0f);
           //ldownarm.transform.localScale = new Vector3(1.0f, 1.54f * (drscale / scale), 1.0f);
           //rdownarm.transform.localScale = new Vector3(1.0f, 1.54f * (drscale / scale), 1.0f);
         }
--- Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/PointListAnnotation.cs
+++ Packages/com.github.homuler.mediapipe/Runtime/Scripts/Unity/Annotation/PointListAnnotation.cs
@@ -18,11 +18,12 @@ namespace Mediapipe.Unity
   {
     [SerializeField] private Color _color = Color.green;
     [SerializeField] private float _radius = 15.0f;
-    public static float zPoint = 80f;
+    public static float zPoint = 90f;
     public static Vector3[] point =new Vector3[33];
     public GameObject canvas;
     public float foot;
     public static float legscale;
+    public static float armscale;
     public GameObject cube;
     public GameObject cube2;
     public GameObject cube3;
@@ -82,25 +83,21 @@ namespace Mediapipe.Unity
 
     void targetPosition(IList<NormalizedLandmark> a)
     {
-      point [23] = new Vector3(a[23].X, a[23].Y, a[23].Z);
-      point[24] = new Vector3(a[24].X, a[24].Y, a[24].Z);
-      point[27] = new Vector3(a[27].X, a[27].Y, a[27].Z);
+      point[23] = new Vector3(a[23].X, a[23].Y, a[23].Z); point[24] = new Vector3(a[24].X, a[24].Y, a[24].Z); point[27] = new Vector3(a[27].X, a[27].Y, a[27].Z);
+      point[11] = new Vector3(a[11].X, a[11].Y, a[11].Z); point[13] = new Vector3(a[13].X, a[13].Y, a[13].Z); point[15] = new Vector3(a[15].X, a[15].Y, a[15].Z);
+      point[12] = new Vector3(a[12].X, a[12].Y, a[12].Z);
       legscale = Vector3.Magnitude(point[23] - point[27]) / Vector3.Magnitude(point[23] - point[24]);
+      armscale = (Vector3.Magnitude(point[15] - point[13]) + Vector3.Magnitude(point[11] - point[13])) / Vector3.Magnitude(point[11] - point[12]);
       for (int i = 0; i < 33; i++)
       {
         point[i] = new Vector3((0.5f - a[i].X) * 2440 * 0.04f, (0.5f - a[i].Y) * 1275 * 0.04f, zPoint); //좌표위치와 똑같이 수정, 캔버스 크기가 바뀌더라도 고치면 안됨.
                                                                                               //좌표의 위치를 결정짓는 실제 스크린 크기는 변하지 않으므로 
       }
-      //point[11].z = foot;
-      //point[12].z = foot;
-      //point[23].z = foot;
-      //point[24].z = foot;//팔다리 위치를 결정시켜줌으로 중요
-      for (int i = 13; i < 23; i++)
+      Debug.Log("point[15]:" + point[15]);
+      for (int i = 15; i < 23; i++)
       {
-        point[i].z = 85.0f;
+        point[i].z = zPoint + (25.0f * (a[i].Z + 0.3f));//z값 받아서 그것을 화면에 반영했을때 이상적인 비율 곱함
       }
-      point[29].z = 85.0f;
-      point[30].z = 85.0f;
     }
 
     public void Draw(IList<NormalizedLandmark> targets, bool visualizeZ = true)
